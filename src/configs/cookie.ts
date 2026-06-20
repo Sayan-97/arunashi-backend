26c0104b49694f72cbe82e43ab788aa5a62b8505
Sayan-97 <sdey4232@gmail.com>
2026-06-20 22:39:52 +0530

    fix: auth fixed and final changes before test

--- src/configs/cookie.ts
+++ src/configs/cookie.ts
@@ -5,7 +5,7 @@ export const accessTokenCookieOptions: CookieOptions = {
 	httpOnly: true,
 	secure: env.NODE_ENV === "production",
 	sameSite: "lax",
-	maxAge: 24 * 60 * 60 * 1000, // 24 hours
+	maxAge: 15 * 60 * 1000, // 15 minutes
 };
 
 export const refreshTokenCookieOptions: CookieOptions = {

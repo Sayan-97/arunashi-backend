b43777fa6cb1a893f871e3c7528d6d4d780b8958
Sayan-97 <sdey4232@gmail.com>
2026-06-21 00:22:09 +0530

    chore: minimal changes made

--- src/validations/registration.validation.ts
+++ src/validations/registration.validation.ts
@@ -6,6 +6,10 @@ export const registerSchema = z.object({
 	company: z.string().nonempty("Company is required"),
 	phone: z.string().nonempty("Phone is required"),
 	address: z.string().nonempty("Address is required"),
+	city: z.string().nonempty("City is required"),
+	state: z.string().nonempty("State is required"),
+	country: z.string().nonempty("Country is required"),
+	zipcode: z.string().nonempty("Zip code is required"),
 	pressTitle: z.string().nonempty("Press title is required"),
 });
 
